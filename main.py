2e0d48bb93bf405982b99f564f33a4159e375415
EvilArthur <thebestmail20@gmail.com>
2020-01-09 18:08:46 +0200

    random circle

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import sys
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 from PyQt5.QtCore import Qt
 from PyQt5 import uic
+from UI import Ui_MainWindow
 from PyQt5.QtGui import QPainter, QColor
 from random import randint
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
@@ -22,7 +23,7 @@ class MyWidget(QMainWindow):
         qp.begin(self)
         if self.flag:
             d = randint(1, 300)
-            qp.setBrush(Qt.yellow)
+            qp.setBrush(QColor(randint(1, 255), randint(1, 255), randint(1, 255)))
             qp.drawEllipse(140, 140, d, d)
         qp.end()
 
